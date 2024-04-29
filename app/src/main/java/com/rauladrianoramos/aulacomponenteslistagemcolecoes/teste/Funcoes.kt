2166a495b8221a06fe0c3e4dbd435c1a4666fbd4
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-29 13:24:33 -0300

    funcoes/metodos por parametro

---
+++ app/src/main/java/com/rauladrianoramos/aulacomponenteslistagemcolecoes/teste/Funcoes.kt
@@ -0,0 +1,45 @@
+package com.rauladrianoramos.aulacomponenteslistagemcolecoes.teste
+
+class Matematica{
+
+    //Metodo - está acociado a uma classe
+    fun somar(n1: Int, n2:Int) : Int {
+        return n1 +n2
+    }
+}
+
+//função
+fun somar(n1: Int, n2:Int) : Int {
+    return n1 +n2
+}
+
+fun calcular( funcao: (Int,Int) ->Int){
+
+    val retorno = funcao(10,10)
+    println(retorno)
+}
+
+fun main(){
+
+    //usando metodo da classe matematica
+    val matematica = Matematica()
+    var retorno = matematica.somar(10,5)
+    println(retorno)
+
+    //usando funcao
+    retorno = somar(10,8)
+    println(retorno)
+
+    //passando função por parametro
+    calcular(::somar)
+
+    //passando metodo por parametro
+    calcular(matematica::somar)
+
+
+
+
+
+
+
+}
